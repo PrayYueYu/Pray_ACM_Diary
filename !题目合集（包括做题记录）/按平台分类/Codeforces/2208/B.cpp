49e6912cc53167ffb49318a0f0623644726ed9fd
PrayYueYu <1944215344@qq.com>
2026-03-20 09:27:34 +0800

    Create B.cpp

---
+++ !题目合集（包括做题记录）/按平台分类/Codeforces/2208/B.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define int long long
+const int N = 2e6 + 10, mod = 998244353, INF = 1e18;
+
+int read() {
+	int x = 0, f = 1;
+	char ch = getchar();
+	while(ch < '0' || ch > '9') {
+		if(ch == '-') f = -1;
+		ch = getchar();
+	}
+	while(ch >= '0' && ch <= '9') {
+		x = x * 10 + ch - '0';
+		ch = getchar();
+	}
+	return x * f;
+}
+void solve() {
+
+}
+signed main() {
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	int T = read();
+	while(T--) {
+		solve();
+//		init();
+	}
+	return 0;
+}
+/*
+
+*/
